8db4b02a7e96c30d85ecf1e9d821d4a62090f3a9
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-06-20 16:51:44 -0400

    sdfsdf
    sdfsdfsdf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path= require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.Port || 8080);
+app.listen(8080);
 
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname + '/dist/index.html'));
